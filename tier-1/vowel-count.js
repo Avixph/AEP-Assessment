8a32738f3e4a6e3937a8bb95b128b8f74392b1a2
Angel B. Fernandez <25628695+Avixph@users.noreply.github.com>
2024-07-30 21:56:00 +0000

    Completed the Vowel Counting question.

--- tier-1/vowel-count.js
+++ tier-1/vowel-count.js
@@ -3,5 +3,21 @@
 @returns {number}
 */
 module.exports = function(str){
-  //Solution goes here
+  // console.log(str)
+  let count = 0
+  // const chars = str.split("")
+  // const vowels = ['a', 'e', 'i', 'o', 'u']
+  // for (let i = 0; i < chars.length; i++) {
+  //   const char = chars[i];
+  //   if (vowels.includes(char)) {
+  //     count++
+  //   }
+  // }
+  const vowels = new Set(['a', 'e', 'i', 'o', 'u'])
+  for (const char of str) {
+    if (vowels.has(char)) {
+      count++
+    }
+  }
+  return count
 }
